b41b9180e2456232cc4d876918c4f8079d7301c8
Nathan Dunfield <nathan@dunfield.info>
2023-03-09 21:48:00 -0600

    Disable Jekyll

--- update_from_wheel.py
+++ update_from_wheel.py
@@ -3,15 +3,19 @@ import os
 import shutil
 import zipfile
 
+work_dir = 'docs-extract-scratch'
 wheel_contents = zipfile.ZipFile(sys.argv[1])
-for dir_name in ['docs', 'docs-extract-scratch']:
-    if os.path.exists(dir_name):
-        shutil.rmtree(dir_name)
-    
+if os.path.exists(work_dir):
+    shutil.rmtree(work_dir)
+
 for file_name in wheel_contents.namelist():
     if file_name.startswith('snappy/doc'):
-        wheel_contents.extract(file_name, 'docs-extract-scratch')
+        wheel_contents.extract(file_name, work_dir)
 
+keepers = ['CNAME', '.nojekyll']
+for file_name in keepers:
+    os.rename('docs/' + file_name, work_dir + '/snappy/doc/' + file_name)
+
+shutil.rmtree('docs')
 os.rename('docs-extract-scratch/snappy/doc', 'docs')
 shutil.rmtree('docs-extract-scratch')
-
